f85d525106d3000532457397bc5751b910e04be6
Carrington Jones <cjones6096@greenvilleschools.us>
2017-08-25 09:59:18 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Introduction here.
+ * 
+ * @author (Carrington Jones) 
+ * @version (August 22, 2017)
+ */
+public class Introduction
+{
+    public static void main(String[] args)
+    {
+        //here are some facts about me
+        System.out.println("My name is Carrington Jones");
+        System.out.println("I was born in Greenville.");
+        System.out.println("I went to Beck Academy.");
+        System.out.println("My Sister, Madeline, is 22.");
+        System.out.println("I like to eat and chill.");
+        
+        //now store some numbers 
+        int year = 2017;
+        int age = 17;
+        
+        //What happens here?
+        System.out.println("year - age");
+        System.out.println(year - age);
+        System.out.println("I am " + age + " years old.");
+    }
+}
+    
+
+
